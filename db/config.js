4ac05b920da7a645dcb526703f5d87c9d6e439df
alexFiorenza <alexxdfiorenza@gmail.com>
2021-03-27 11:48:44 -0300

    Deploy test mongouri heroku

--- db/config.js
+++ db/config.js
@@ -6,6 +6,7 @@ module.exports = async (url) => {
       url,
       { useUnifiedTopology: true, useNewUrlParser: true },
       (err) => {
+        console.log(url);
         return console.log(
           `Succesfully connected to: ${chalk.green('MongoDb')}`
         );
